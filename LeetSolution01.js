c49b60182ea6fb73fd5e35cdfa6d96c134fa8363
RubenV-Dev <r.will16@hotmail.com>
2020-10-25 17:27:27 -0400

    adding comments to solution

--- LeetSolution01.js
+++ LeetSolution01.js
@@ -16,12 +16,20 @@ let nums = [2,7,11,15]
 let target = 9
 
 let twoSum = (nums, target) => {
+    //First we create a hash that will store the (target - the current value in the iteration) as a key with a value of the current i
+    //because this will allow us to know what value we are looking for in order to reach the target number. We store the current i 
+    // so we can remember which place in the array needs which value.
     let valueLocation = {}
 
+    //ideally we are looking to just iterate the array given once for a time complexity of O(n) where n is the number of items in the array
     for (let i = 0; i < nums.length; i++) {
         let currentValue = nums[i]
 
+        //if we do not see a hash entry for the current value we are on, keep adding to the hash the difference between target 
+        //and the currentvalue as well as the current index i. Otherwise we take the stored i value in the hash and return it
+        // along with the current i value.
         if (valueLocation[currentValue] != undefined) {
+            console.log(`Im logging.... ${[valueLocation[currentValue], i]}`)
             return [valueLocation[currentValue], i]
         }
 
