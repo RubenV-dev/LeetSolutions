f0a41b6f0352c9f415e03098d49c1b8867f4d0d9
RubenV-Dev <r.will16@hotmail.com>
2020-11-01 19:29:00 -0500

    added solution

--- reverseInteger-07.js
+++ reverseInteger-07.js
@@ -5,3 +5,16 @@
 // the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this 
 // problem, assume that your function returns 0 when the reversed integer overflows.
 
+let reverse = (x) => {
+    const limit = 2 ** 31  
+    const sign = x < 0? -1 : 1
+    let string = Math.abs(x).toString()
+    let returnedString = ""
+
+    for (let i = 0; i < string.length; i++) {
+        returnedString = string.charAt(i) + returnedString 
+    }
+
+    return Number(returnedString) > limit? 0 : Number(returnedString) * sign
+
+}
