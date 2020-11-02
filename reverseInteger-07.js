c5475d470717390a60b032c7969f87be2dcd67f1
RubenV-Dev <r.will16@hotmail.com>
2020-11-01 19:32:32 -0500

    added comments

--- reverseInteger-07.js
+++ reverseInteger-07.js
@@ -7,14 +7,21 @@
 
 let reverse = (x) => {
     const limit = 2 ** 31  
+    //we first take the given value and save whether or not it was a negative or positive
+    // integer.
     const sign = x < 0? -1 : 1
+    //we then take the absolute value eliminating the sign and then stringify the value.
     let string = Math.abs(x).toString()
+    
     let returnedString = ""
-
+    //we will iterate through the string and constantly add each digit to the beginning
+    // of the returnedString.
     for (let i = 0; i < string.length; i++) {
         returnedString = string.charAt(i) + returnedString 
     }
-
+    //finally we check if there was any overflow in the value we will return 0,
+    // if there isnt then we will return the number form of our return string and convert
+    // to the original sign by multiplying by sign.
     return Number(returnedString) > limit? 0 : Number(returnedString) * sign
 
 }
